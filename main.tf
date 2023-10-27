0c38d1c1f7d6f087a8bb0754b480bcc8cad527fe
megha <anmeghan04@gmail.com>
2023-10-27 20:51:44 +0530

    index

--- main.tf
+++ main.tf
@@ -52,3 +52,16 @@ resource "aws_s3_object" "profile" {
   source = "profile.png"
   acl = "public-read"
 }
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.mybucket.id
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  depends_on = [aws_s3_bucket_acl.example]
+}
